0cdca66f7ccfa01b29e0f75f79b7bccb44d24846
maki <hoge@hoge.local>
2016-07-03 14:06:42 +0900

    first commit

---
+++ DSCR_Firewall.psd1
@@ -0,0 +1,31 @@
+﻿@{
+
+# Script module or binary module file associated with this manifest.
+RootModule = 'DSCR_Firewall.psm1'
+
+DscResourcesToExport = '*'
+
+# Version number of this module.
+ModuleVersion = '1.0'
+
+# ID used to uniquely identify this module
+GUID = '463baeaa-9344-45a8-86a5-e3abca1c61eb'
+
+# Author of this module
+Author = 'Maki@hplus'
+
+# Company or vendor of this module
+CompanyName = ''
+
+# Copyright statement for this module
+Copyright = ''
+
+# Description of the functionality provided by this module
+# Description = ''
+
+# Minimum version of the Windows PowerShell engine required by this module
+PowerShellVersion = '5.0'
+
+# Name of the Windows PowerShell host required by this module
+# PowerShellHostName = ''
+}
---
+++ DSCR_Firewall.psm1
@@ -0,0 +1,86 @@
+﻿enum Ensure
+{
+    Absent
+    Present
+}
+
+enum FwProfile
+{
+    Domain = 1
+    Private = 2
+    Public = 4
+}
+
+[DscResource()]
+class cFirewall
+{
+    [DscProperty(Key)]
+    [ValidateSet("All","Domain","Private","Public")]
+    [string]$Profile
+
+    [DscProperty(Mandatory)]
+    [Ensure]$Ensure
+
+    [DscProperty(NotConfigurable)]
+    [hashtable[]]$CurrentSet
+
+    # リソースの適切な状態を設定します。
+    [void] Set()
+    {
+        if("All" -eq $this.Profile){
+            $ProfileSet = @([Enum]::GetNames([FwProfile]))
+        }
+        else{
+            $ProfileSet = @($this.Profile)
+        }
+        # Set-NetFirewallProfile -Enabled ($this.Ensure -eq [Ensure]::Present) # Win7では使えないCmdlet
+        $fw = New-Object -ComObject hnetcfg.fwpolicy2
+        @($ProfileSet) | where {[Enum]::IsDefined([FwProfile], $_)} | foreach{
+            try{
+                $fw.FirewallEnabled([FwProfile]$_) = $this.Ensure
+            }catch{}
+        }
+    }        
+    
+    # リソースの状態が適切かどうかをテストします。
+    [bool] Test()
+    {   
+        if($this.Ensure -eq [Ensure]::Absent){
+            return ($this.Ensure -eq $this.Get().Ensure)
+        }
+        else{
+            return [bool]!($this.Get().CurrentSet | where {$_.Enabled -eq $false})
+        }
+    }
+
+    # リソースの現在の状態を取得します。
+    [cFirewall] Get()
+    {
+        $Ret = New-Object $this.GetType()
+        $Ret.Profile = $this.Profile
+
+        if("All" -eq $this.Profile){
+            $ProfileSet = @([Enum]::GetNames([FwProfile]))
+        }
+        else{
+            $ProfileSet = @($this.Profile)
+        }
+
+        $Ret.CurrentSet = @()
+        $fw = New-Object -ComObject hnetcfg.fwpolicy2
+
+        $Ret.Ensure = [Ensure]::Absent
+        @($ProfileSet) | where {[Enum]::IsDefined([FwProfile], $_)} | foreach {
+            try{
+                if($fw.FirewallEnabled([FwProfile]$_)){
+                    $Ret.CurrentSet += @{Profile=$_; Enabled=$true}
+                    $Ret.Ensure = [Ensure]::Present
+                }
+                else{
+                    $Ret.CurrentSet += @{Profile=$_; Enabled=$false}
+                }
+            }catch{}
+        }
+        return $Ret 
+    }    
+}
